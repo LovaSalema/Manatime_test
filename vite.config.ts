da0166f45b5689f453bc784dfe188bc4bf54d3eb
Lova Salema <hei.salema@gmail.com>
2023-11-06 14:45:10 +0300

    manatime

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/",
+  base: "/manatime_test/",
   build: {
     rollupOptions: {
       external: ['react/jsx-runtime'],
